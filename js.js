486e43ad5985285ea0543aef6a83a2b6d31c4a98
Corme21 <jesusallo.21@gmail.com>
2023-09-13 09:19:27 +0200

    Hola js

---
+++ js.js
@@ -0,0 +1,14 @@
+let num1 = 2;
+let num2 = 3;
+let suma = num1 + num2;
+
+
+//Para mandar mensajes
+let nombre = prompt("Introduce el nombre:");
+
+let apellidos = prompt("Introduce los apellidos:");
+
+let edad = prompt("Introduce la edad:");
+
+document.write(`<strong>Nombre: \"${nombre}\"</br>Apellidos: ${apellidos}</br>Edad: ${edad}</strong>`);
+
