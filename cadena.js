df9907b64a587c808e900a2ff49d7cd05416abfb
Corme21 <jesusallo.21@gmail.com>
2023-09-13 16:40:07 +0200

    Cadena

---
+++ cadena.js
@@ -0,0 +1,20 @@
+
+document.write("El acceso a la ruta C:\\\\usuario\\ tarda 1\`23\", algo considerado \"lento\" en la actualidad");
+
+var numBinrio = 0b1111;
+
+var numHexa = 0x26a;
+
+var numOctl = 0o111;
+
+console .log(numBinrio);
+
+console .log(numHexa);
+
+console .log(numOctl);
+
+DivisionPorCero = 23/0;
+
+outraOp = DivisionPorCero + 12;
+
+console.log(outraOp);
