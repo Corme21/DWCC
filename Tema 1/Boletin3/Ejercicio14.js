9c5f48edf2f61be3edd2eaec3be8de42b559eb0c
Corme21 <jesusallo.21@gmail.com>
2023-09-21 13:22:22 +0200

    Ejercicios

---
+++ Tema 1/Boletin3/Ejercicio14.js
@@ -0,0 +1,6 @@
+//Debuxar por pantalla con asteriscos o número de liñas que introduce o usuario
+let lineas = prompt("Introduce la cantidad de lineas a dibujar");
+
+for(let i = 0; i < lineas; i++){
+    
+}
