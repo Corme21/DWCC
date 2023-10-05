81fc67c0e0c654c1c626b5cf3fd5dcd29b6c9a91
Corme21 <128130363+Corme21@users.noreply.github.com>
2023-10-05 17:37:53 +0200

    Add files via upload

---
+++ Tema 1/Boletin4/Ejercicio8.js
@@ -0,0 +1,8 @@
+/*
+Escribe un programa que tome unha cadea e elimine todos os 
+espazos en branco ao principio e ao final desta.
+*/
+
+let cadena = prompt("Escribe una frase");
+
+console.log(cadena.trim());
