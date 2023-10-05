b07703eb0c38f2b3d7c5b5339af7f43d6d07d697
Corme21 <128130363+Corme21@users.noreply.github.com>
2023-10-05 17:43:32 +0200

    Delete Ejercicio8.js

--- Ejercicio8.js
+++
@@ -1,8 +0,0 @@
-/*
-Escribe un programa que tome unha cadea e elimine todos os 
-espazos en branco ao principio e ao final desta.
-*/
-
-let cadena = prompt("Escribe una frase");
-
-console.log(cadena.trim());
