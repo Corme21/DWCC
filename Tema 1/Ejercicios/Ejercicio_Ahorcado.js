6eb1b5ccc6800c03e62454a02809928e517451d5
Corme21 <jesusallo.21@gmail.com>
2023-11-06 09:58:15 +0100

    Ejercicios

--- Tema 1/Ejercicios/Ejercicio_Ahorcado.js
+++ Tema 1/Ejercicios/Ejercicio_Ahorcado.js
@@ -1,2 +1,41 @@
 //Juego del ahorcado
 
+let clave = prompt("Escribe la palabra oculta");
+
+let pista = prompt("Escribe una pista");
+
+let tamaño = clave.length;
+
+let intentos = tamaño + 4;
+
+console.log("Palabra escrita");
+console.log(`Tienen ${intentos} intentos`);
+console.log(`El tamaño de la palabra es ${tamaño}`);
+console.log(`La pista es "${pista}"`);
+console.log("Jugadores, comiencen");
+
+while(sigue = true){
+    console.log(`Quedan ${intentos} intentos`);
+    
+    let letra = prompt("Escribe una letra");
+
+    function comprobarLetra(letra){
+        for(let i = 0; i < clave.length; i++){
+            if(clave.includes(letra)){
+                
+            }else{
+                intentos--;
+            }
+        }
+    }
+    
+    comprobarLetra(letra);
+
+    if(intentos == 0){
+        console.log("Has perdido");
+        console.log(`La palabra era ${clave}`);
+        sigue = false;
+        break;
+    }
+}
+
