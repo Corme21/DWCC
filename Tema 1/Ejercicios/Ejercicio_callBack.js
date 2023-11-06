b86caba07f3babbd73cbe1e8783f76a552d034e2
Corme21 <jesusallo.21@gmail.com>
2023-11-06 10:18:18 +0100

    Ejercicios

---
+++ Tema 1/Ejercicios/Ejercicio_callBack.js
@@ -0,0 +1,15 @@
+/*
+Escribe unha función chamada addArray que tome unha matriz de numeros como primeiro argumento
+e unha funcion de devolucion de chamada como segundo argumento. A funcion de devolucion de 
+chamada empregarase para realizar unha operación en cada elemento da matriz e devolver a suma
+total.
+Por exemplo: function sumarArray(arr, callback)
+*/
+
+let array = [1, 2, 3, 4, 5];
+
+function addArray(argumento1, argumento2){
+    
+}
+
+addArray();
