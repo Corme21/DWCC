70e0412f1dbbd95bef8275ef2d6dc5c618b516f4
Corme21 <jesusallo.21@gmail.com>
2023-11-09 14:24:14 +0100

    Ejercicios

--- Tema 1/Ejercicios/Ejercicios Callback/Ejercicio4.js
+++ Tema 1/Ejercicios/Ejercicios Callback/Ejercicio4.js
@@ -1,9 +1,22 @@
 /*
 Facer un programa que reciba cadeas de texto nunha funcion ata que o usuario decida para de 
-envialas, e que estas cadeas se pase aa outra función creando un texto final
+envialas, e que estas cadeas se pase a outra función creando un texto final
 */
+let cadena1 = prompt("Escribe una cadena");
 
 while(sigue != false){
-    
+    function guardarCadenas(cadena1, cadena2){
+        let cadena2 = prompt("Escribe otra cadena");
+        let continuar = prompt("Quieres escribir mas cadenas?");
+
+        if(continuar == "si"){
+            sigue = true;
+        }else{
+            sigue = false;
+        }
+    }
+}
+
+function textoFinal(){
     
 }
