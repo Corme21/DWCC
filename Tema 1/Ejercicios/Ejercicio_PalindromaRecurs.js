d72faa31bf9b5ee89a0de6cb4a34182b6166bb0e
Corme21 <jesusallo.21@gmail.com>
2023-11-14 14:13:06 +0100

    Ejercicios

---
+++ Tema 1/Ejercicios/Ejercicio_PalindromaRecurs.js
@@ -0,0 +1,10 @@
+//Si una palabra es palindroma usando recursividad
+
+let palabra = prompt("Escribe una palabra");
+
+let revers = "";
+const palindroma = (palabra, revers) =>{
+    
+}
+
+palindroma(palabra, revers);
