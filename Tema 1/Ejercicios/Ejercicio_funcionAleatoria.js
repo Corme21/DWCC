3eb8d6427d3e571dce0bc868a422744c9faa005c
Corme21 <jesusallo.21@gmail.com>
2023-11-14 13:35:55 +0100

    Ejercicios

--- Tema 1/Ejercicios/Ejercicio_funcionAleatoria.js
+++ Tema 1/Ejercicios/Ejercicio_funcionAleatoria.js
@@ -4,20 +4,18 @@ debe pedir desde teclado.
 Facer co funcions frecha e recursividade.
 */
 
-let maximo = Number(prompt("Escribe el maximo de elementos"));
-
 let minimo = Number(prompt("Escribe el minimo de elementos"));
 
 let arrayRandom = [];
 
-const aleatorioArray = (maximo, arrayRandom, minimo)=>{
-    if(minimo == maximo){
-        console.log(arrayRandom);
-    }else{
-        var numRandom = Math.floor(Math.random() * 100);
+const aleatorioArray = ()=>{
+    var numRandom = Math.floor(Math.random() * 100);
+    if(arrayRandom.length < minimo){
         arrayRandom.push(numRandom);
-        return aleatorioArray(maximo, arrayRandom, (minimo+1));
+        aleatorioArray();
     }
 }
 
-aleatorioArray(maximo, arrayRandom, minimo);
+aleatorioArray();
+
+console.log(arrayRandom);
