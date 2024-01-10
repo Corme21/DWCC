60b7816cbcc8cd940c99828a361f18c53c11e10a
Corme21 <jesusallo.21@gmail.com>
2024-01-10 16:29:28 +0100

    Exercicios

---
+++ Tema 2/Ejercicios/Expresion_Regular/expresiones.js
@@ -0,0 +1,10 @@
+//Crear expresiones regulares para
+
+//Recibir exclusivamente numeros do 0 ao 4
+
+//Recibir exclusivamente cadeas que comencen por maiuscula
+
+//Recibir exclusivamente cadeas que rematen por "dw"
+
+//Impedir que se reciban numeros que conteñan 0
+
