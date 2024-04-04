8387827095ffe89c3f441e985b522017dfeeb696
Corme21 <jesusallo.21@gmail.com>
2024-04-04 13:45:15 +0200

    Exercicio

---
+++ Tema 4/Exercicios/API/Canvas/dibujar.js
@@ -0,0 +1,36 @@
+var c = document.getElementById("myCanvas");
+var linea1 = c.getContext("2d");
+linea1.strokeStyle = "red";
+linea1.moveTo(60, 0);
+linea1.lineTo(30, 60);
+linea1.stroke();
+
+var linea2 = c.getContext("2d");
+linea2.strokeStyle = "red";
+linea2.moveTo(60, 0);
+linea2.lineTo(90, 60);
+linea2.stroke();
+
+var linea3 = c.getContext("2d");
+linea3.strokeStyle = "red";
+linea3.moveTo(60, 120);
+linea3.lineTo(30, 60);
+linea3.stroke();
+
+var linea4 = c.getContext("2d");
+linea4.strokeStyle = "red";
+linea4.moveTo(60, 120);
+linea4.lineTo(90, 60);
+linea4.stroke();
+
+var linea5 = c.getContext("2d");
+linea5.strokeStyle = "red";
+linea5.moveTo(60, 120);
+linea5.lineTo(120, 120);
+linea5.stroke();
+
+var linea5 = c.getContext("2d");
+linea5.strokeStyle = "red";
+linea5.moveTo(200, 120);
+linea5.lineTo(120, 120);
+linea5.stroke();
