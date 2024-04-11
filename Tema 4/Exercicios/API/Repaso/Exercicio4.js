d36e2b9c425df3063f8566801bc4bcd060f6107e
Corme21 <jesusallo.21@gmail.com>
2024-04-11 10:48:42 +0200

    Exercicios

--- Tema 4/Exercicios/API/Repaso/Exercicio4.js
+++ Tema 4/Exercicios/API/Repaso/Exercicio4.js
@@ -7,7 +7,16 @@ Unha vez almacenados, pide por teclado o nome que queiras eliminar. Se o atopa, 
 atopa deberás indicar que non foi atopado. 
 */
 
-let nomeBarco = prompt("Introduce o nome do barco");
+for(let i = 0; i < 5; i ++){
+    let nomeBarco = prompt("Introduce o nome do barco");
 
-let anoConstr = prompt("Introduce o ano de construccion");
+    let anoConstr = prompt("Introduce o ano de construccion");
+
+    var info = {Nome_Barco: nomeBarco, Porto_Base: portoBase, 
+        Ano_Constr: anoConstr, eslora: eslora};
+
+    localStorage.setItem("clave", JSON.stringify(info));
+
+    
+}
 
