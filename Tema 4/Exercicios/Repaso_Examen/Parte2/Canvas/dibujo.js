13d4a2b48176f76d1bc6eec54a32a3c138309eee
Corme21 <jesusallo.21@gmail.com>
2024-04-16 13:41:13 +0200

    Exercicio

--- Tema 4/Exercicios/Repaso_Examen/Parte2/Canvas/dibujo.js
+++ Tema 4/Exercicios/Repaso_Examen/Parte2/Canvas/dibujo.js
@@ -1,10 +1,16 @@
 function dibujarCarita(){
+    let fecha = new Date();
+
+    let data = fecha.toDateString();
+
     let lienzo = document.getElementById("lienzo");
     if (lienzo.getContext){
         let mano = lienzo.getContext("2d");
+        
         mano.beginPath();
         mano.arc(75,75,50,0,Math.PI*2,true);
         mano.stroke();
+        mano.fillText()
     }
 }
 dibujarCarita();
