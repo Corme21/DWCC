6a6e3f4e7a7fd74829d168f5256ce5cefeba7679
Corme21 <jesusallo.21@gmail.com>
2024-04-17 10:23:32 +0200

    Exercicios

--- Tema 4/Exercicios/Boletin/Usuarios/principal.js
+++ Tema 4/Exercicios/Boletin/Usuarios/principal.js
@@ -1,2 +1,49 @@
 import { agregar, eliminar, buscar } from "./usuarios.js";
 
+/*
+  console.log('--- MENU ---');
+  console.log('1. Agregar usuario');
+  console.log('2. Eliminar usuario');
+  console.log('3. Buscar usuario');
+  console.log('4. Salir');
+
+    var opcion = prompt("Elige una opcion");
+
+  switch (opcion) {
+    case '1':
+      rl.question('Ingrese el id de usuario: ', (idusuario) => {
+        rl.question('Ingrese el nombre de usuario: ', (nome) => {
+          usuarios.agregarUsuario(idusuario, nome);
+          mostrarMenu();
+          solicitarOpcion();
+        });
+      });
+      break;
+    case '2':
+      rl.question('Ingrese el id de usuario a eliminar: ', (idusuario) => {
+        usuarios.eliminarUsuario(idusuario);
+        mostrarMenu();
+        solicitarOpcion();
+      });
+      break;
+    case '3':
+      rl.question('Ingrese el id de usuario a buscar: ', (idusuario) => {
+        usuarios.buscarUsuario(idusuario);
+        mostrarMenu();
+        solicitarOpcion();
+      });
+      break;
+    case '4':
+      rl.close();
+      break;
+    default:
+      console.log('Opción inválida');
+      mostrarMenu();
+      solicitarOpcion();
+      break;
+  }
+}
+
+mostrarMenu();
+solicitarOpcion();
+*/
--- Tema 4/Exercicios/Boletin/Usuarios/usuarios.js
+++ Tema 4/Exercicios/Boletin/Usuarios/usuarios.js
@@ -9,6 +9,15 @@ let agregar = (idUsuario, nombre) =>{
 
 let eliminar = (idUsuario) =>{
     mapa.delete(idUsuario);
+    /*
+    if (usuarios.has(idusuario)) {
+    let nome = usuarios.get(idusuario);
+    usuarios.delete(idusuario);
+    console.log(`Usuario eliminado: ${nome}`);
+  } else {
+    console.log("El usuario no existe");
+  }
+    */
 }
 
 let buscar = (idUsuario) =>{
@@ -17,6 +26,15 @@ let buscar = (idUsuario) =>{
     }else{
         console.log("Usuario no encontrado");
     }
+
+    /*
+if (usuarios.has(idusuario)) {
+    let nome = usuarios.get(idusuario);
+    console.log(`Usuario encontrado: ${nome}`);
+  } else {
+    console.log("El usuario no existe");
+  }
+    */
 }
 
 export{agregar, eliminar, buscar}
